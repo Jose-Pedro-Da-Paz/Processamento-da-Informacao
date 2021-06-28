d7af7aef2b017a694b34465cdfb16cc06567048d
José Pedro Da Paz <74993512+Jose-Pedro-Da-Paz@users.noreply.github.com>
2021-06-28 15:24:54 -0300

    Create EP2.3.py

---
+++ EP2.3.py
@@ -0,0 +1,13 @@
+temp =  float(input())
+
+if temp >= 250:
+  print("Muito Alta")
+elif temp >= 200:
+  print("Alta")
+elif temp >= 30:
+  print("Normal")
+elif temp >= (-20):
+  print("Baixa")
+else:
+  print("Muito Baixa")
+  
